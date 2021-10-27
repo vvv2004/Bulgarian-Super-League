d1f9e064e4f23332cf60b59878779c4f3add8854
vvv2004 <viktorvalentinov14@abv.bg>
2021-10-27 10:07:31 +0300

    Created Person And Nationality Class
    Opisanie

---
+++ src/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String name;
+    int age;
+    Nationality nationality;
+
+    public Person(String name, int age, Nationality nationality) {
+        this.name = name;
+        this.age = age;
+        this.nationality = nationality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
