f296d23d4d5e10eb1edee3e8bfed61c01a24ceec
vvv2004 <viktorvalentinov14@abv.bg>
2021-11-14 21:30:02 +0200

    Fixed Reader

---
+++ Java/src/ReaderWriter/Reader.java
@@ -0,0 +1,51 @@
+package ReaderWriter;
+import Classes.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Reader {
+    private String path;
+    private String teamInfoFile;
+    Scanner scan;
+
+    public Reader(){
+        path = "../Teams";
+        teamInfoFile  = "team_info.txt";
+    }
+
+    public String getTeamInfo(String conference, String team) throws IOException{
+        String path = this.path + '/' + conference + '/' + team + "/team_info.txt";
+
+        String teamInfo = "";
+
+        scan = new Scanner(new File(path));
+
+        while(scan.hasNextLine()){
+            teamInfo += scan.nextLine();
+            teamInfo += '\n';
+        }
+
+        return teamInfo;
+    }
+
+    public String getSpecificTeamInfo(String teamInfo, String typeOfInfo) {
+        String output = "";
+
+        Scanner scan = new Scanner(teamInfo);
+
+        while(scan.hasNextLine()){
+            String dataInRow = scan.nextLine();
+
+            if(dataInRow.contains(typeOfInfo)){
+                output += dataInRow;
+                break;
+            }
+        }
+
+        String[] separateData = output.split("=");
+
+        return separateData[1];
+    }
+}
--- Java/src/TestTeams.java
+++ Java/src/TestTeams.java
@@ -1,5 +1,26 @@
 public class TestTeams {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         
+=======
+        Player testPlayer = new Player("Ivan", 16, "Bulgarian", Position.MIDFIELDER, 75);
+
+        System.out.println(testPlayer.getPosition());
+
+        Reader read = new Reader();
+        String teamInfo = "";
+        String goalDifference = "";
+
+        try {
+            teamInfo = read.getTeamInfo("WEST", "Bdin Vidin");
+            goalDifference = read.getSpecificTeamInfo(teamInfo, "goalDifference");
+        }catch(IOException e){
+            System.out.println(e);
+        }
+        System.out.println(teamInfo);
+        System.out.println("\nGoal difference only: " + goalDifference);
+
+
+>>>>>>> Stashed changes
     }
 }
