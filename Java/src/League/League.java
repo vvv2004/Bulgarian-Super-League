3296564284a8fcd7109b423872a03ddf7528d5ae
VelizarAng <nogood945@gmail.com>
2022-01-05 19:27:18 +0200

    Made some league changes

--- Java/src/League/League.java
+++ Java/src/League/League.java
@@ -20,10 +20,64 @@ public class League{
     public Team[] orderTeamsByPoints(){
         Team[] output = teams;
 
-        for(){
-
+        int n = output.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (output[j].getPoints() > output[j + 1].getPoints()) {
+                    Team temp = output[j];
+                    output[j] = output[j + 1];
+                    output[j + 1] = temp;
+                }
+            }
         }
+        return output;
+    }
 
+    public Team[] orderTeamsByGoalsScored(){
+        Team[] output = teams;
+
+        int n = output.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (output[j].getGoalsScored() > output[j + 1].getGoalsScored()) {
+                    Team temp = output[j];
+                    output[j] = output[j + 1];
+                    output[j + 1] = temp;
+                }
+            }
+        }
+        return output;
+    }
+
+    public Team[] orderTeamsByGoalsAllowed(){
+        Team[] output = teams;
+
+        int n = output.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (output[j].getGoalAllowed() > output[j + 1].getGoalAllowed()) {
+                    Team temp = output[j];
+                    output[j] = output[j + 1];
+                    output[j + 1] = temp;
+                }
+            }
+        }
+        return output;
+    }
+
+    public Team[] orderTeamsByGoalDifference(){
+        Team[] output = teams;
+
+        int n = output.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (output[j].getGoalDifference() > output[j + 1].getGoalDifference()) {
+                    Team temp = output[j];
+                    output[j] = output[j + 1];
+                    output[j + 1] = temp;
+                }
+            }
+        }
         return output;
     }
 }
